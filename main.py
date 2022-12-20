6093f58df6d09f0b29a4a63e646d591b62c06d8b
DmitriiMalkov <dmitriy.malkov.02@inbox.ru>
2022-12-20 03:44:53 +0300

    Create Tests

---
+++ main.py
@@ -0,0 +1,77 @@
+import random
+
+
+class SongsQuiz:
+    songs = ["Jingle Bells", "Last Christmas", "Never Gonna Give You Up", "Shape of You",
+             "We Wish You a Merry Christmas"]
+    usedSongs = []
+    i = 5
+    pluses = 0
+    result = ""
+    quizes = 0
+    testpluses = 0
+    file = 0
+    song = "Jingle Bells"
+    TP = []
+    TNTSS = []
+    answer = "Jingle Bells"
+    TRQ = []
+    TRR = 0
+
+    def testPlus(self):
+        if self.answer.lower() == self.song.lower() and self.pluses == (self.testpluses + 1):
+            self.testpluses = self.testpluses + 1
+            self.TP.append(1)
+        elif self.answer.lower() != self.song.lower() and self.pluses == self.testpluses:
+            self.TP.append(1)
+        else:
+            self.TP.append(0)
+
+    def testNotTheSameSong(self):
+        if self.song in self.usedSongs or self.song == "":
+            self.TNTSS.append(0)
+        else:
+            self.TNTSS.append(1)
+            self.usedSongs.append(self.song)
+
+    def testRightQuestion(self):
+        temp = open(self.song + ".txt", "r")
+        if str(temp) == str(self.file):
+            self.TRQ.append(1)
+        else:
+            self.TRQ.append(0)
+
+    def testRightResult(self):
+        if self.result == f"Вы угадали {self.pluses} из {self.i} песен.":
+            self.TRR = 1
+
+    def testsCheck(self):
+        if 0 in self.TP:
+            print("testPlus: unsuccess")
+        else:
+            print("testPlus: success")
+        if 0 in self.TNTSS:
+            print("testNotTheSameSong: unsuccess")
+        else:
+            print("testNotTheSameSong: success")
+        if 0 in self.TRQ:
+            print("testRightQuestion: unsuccess")
+        else:
+            print("testRightQuestion: success")
+        if self.TRR == 1:
+            print("testRightResult: success")
+        else:
+            print("testRightResult: unsuccess")
+
+    def main(self):
+        while self.quizes != 5:
+            self.testNotTheSameSong(obj)
+            self.testRightQuestion(obj)
+            self.testPlus(obj)
+            self.quizes = self.quizes + 1
+        self.testRightResult(obj)
+        self.testsCheck(obj)
+
+
+obj = SongsQuiz
+obj.main(obj)
